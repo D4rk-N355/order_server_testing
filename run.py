cadfa02f43838e6c7b157f3b49aad5ba64b1db32
d4rkn355 <dcdreamchaser01@gmail.com>
2025-12-01 00:34:45 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
-#.\testenv\Scripts\activate.ps1
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
